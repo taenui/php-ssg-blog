c0849775cf8a83b83e17c96ee4ebcdd5678a1716
taenui <76942971+taenui@users.noreply.github.com>
2021-05-09 21:44:43 +0900

    .

--- pf/process/index.js
+++ pf/process/index.js
@@ -3,10 +3,90 @@ gsap.registerPlugin(ScrollTrigger);
 
 const tl1 = gsap.timeline({
   scrollTrigger: {
-    trigger:".part1",
-    markers:false,
-   start:"top 50%",
+    trigger:".part3",
+    markers:true,
+   start:"top 20%",
     end:"bottom top",
     toggleActions: "restart none none none"
   },
+});
+
+
+tl1.from('.part3 > .circle', {
+  y: -100,
+  opacity:0,
+  duration: .8
+});
+
+
+
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger:".part4",
+    markers:true,
+   start:"top 20%",
+    end:"bottom top",
+    toggleActions: "restart none none none"
+  },
+});
+
+
+tl2.from('.part4 > .part4-text', {
+  x: -100,
+  opacity:0,
+  duration: .8
+});
+
+
+const tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger:".part4 > .about",
+    markers:true,
+   start:"top 20%",
+    end:"bottom top",
+    toggleActions: "restart none none none"
+  },
+});
+
+
+tl3.from('.part4 .about-text', {
+  x: -100,
+  opacity:0,
+  duration: .8
+});
+
+
+
+const tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger:".part4 > .port",
+    markers:true,
+   start:"top 20%",
+    end:"bottom top",
+    toggleActions: "restart none none none"
+  },
+});
+
+
+tl3.from('.part4 .port-text', {
+  x: -100,
+  opacity:0,
+  duration: .8
+});
+
+const tl5 = gsap.timeline({
+  scrollTrigger: {
+    trigger:".part4 > .port",
+    markers:true,
+   start:"top 20%",
+    end:"bottom top",
+    toggleActions: "restart none none none"
+  },
+});
+
+
+tl5.from('.part4 .Ex-text', {
+  x: -100,
+  opacity:0,
+  duration: .8
 });
